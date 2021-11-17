6cd9c500b6d5cda4868b5f30ac6febfe7184e078
TL <TL@gmail.com>
2021-11-17 06:42:28 +0530

    Branch with Demo.java Added

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo{
    
-   private static final String URL="http://www.equifox.com/";
+   private static final String URL="http://www.equifox.com/Scores";
 public void doProcess(){
 }
 }
